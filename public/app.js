86fb9e1bc331c3e054109a0e6e539216a82084b8
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-18 18:32:27 -0500

    commiting updated files

---
+++ public/app.js
@@ -0,0 +1,57 @@
+$(document).on("click","#news", function() { 
+$.getJSON("/all", function(data) {
+
+    for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#article").append("<p data-id='" + data[i]._id + "'>");
+        $("#article").append("<p> <h4>" + data[i].title + "<h4><p>");
+        $("#article").append("<p>" + data[i].link + "</p>");
+      }
+    });
+});
+$(document).on("click","p", function() {
+    $("#comment").empty();
+
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/all/" + id
+    })
+    .then(function(data) {
+        console.log(data);
+    $("#comment").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#comment").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#comment").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#comment").append("<button data-id='" + data._id + "' id='savecomment'>Save Comment</button>");
+
+      if(data.comment) {
+          $("#titleinput").val(data.comment.title);
+
+          $("#bodyinput").val(data.comment.body);
+      }
+    });
+});
+
+$(document).on("click", "#savecomment",function() {
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+        method: "POST",
+        url: "/all/" + id,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+        .then(function(data) {
+            console.log(data);
+            $("#comment").empty();
+        });
+
+        $("#titleinput").val("");
+        $("#bodyinput").val("");
+});
---
+++ server.js
@@ -0,0 +1,116 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var logger = require("morgan");
+var db = require("./models");
+var mongojs = require("mongojs")
+var PORT = 3000;
+var app = express();
+var exphbs = require("express-handlebars");
+
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars")
+
+var databaseUrl = "NewsScrape";
+var collections = ["newScraperdb"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+//connecting to Mongo DB
+mongoose.connect("mongodb://localhost/newScraperdb", { useNewURLParser: true });
+
+
+
+app.get("/scrape", function(req,res) {
+    axios.get("https://www.nytimes.com/section/sports/golf").then(function(response) {
+
+        // Load the HTML into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(response.data);
+      
+        // An empty array to save the data that we'll scrape
+        var results = [];
+        
+        // Select each element in the HTML body from which you want information.
+        // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+        // but be sure to visit the package's npm page to see how it works
+        $("article").each(function(i, element) {
+      
+          var title = $(element).children().text();
+          title = title.split("").join(" ").split("\n").join(" ").trim();
+          var link = $(element).find("a").attr("href");
+      
+          // Save these results in an object that we'll push into the results array we defined earlier
+         var scrapedArticles = {
+             title: title,
+             link: link
+         }
+          results.push(scrapedArticles);
+
+          db.Article.create(results) 
+            .then(function(scrapeddb) {
+                console.log(scrapeddb);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+
+          db.Article.insert(scrapedArticles, function(err,data) {
+              if (err) throw err;
+              res.json(data);
+          })
+          
+         });
+         res.send(results);
+        
+        // Log the results once you've looped through each of the elements found with cheerio
+     });
+});
+
+    app.get("/all", function(req,res) {
+        db.Article.find({}, function(err,data) {
+              if(err) throw err;
+              res.json(data);
+            });
+          });
+
+    app.get("/all/:id", function(req,res) {
+        db.Article.findOne({_id: req.params.id})
+
+           // .populate("comments")
+            .then(function(results) {
+                res.json(results)
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+        });
+    app.post("/all/:id", function(req,res) {
+        db.Comment.create(req.body)
+            .then(function(dbComment) {
+                return db.Article.findOneAndUpdate(
+                    {_id: req.params.id},
+                    {$push:{comment:dbComment._id}},
+                    {new: true}
+                )
+            })
+            .then(function(dbArticle) {
+                res.json(dbArticle)
+            })
+            .catch(function(err) {
+                res.json(err)
+            })
+    })
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT)
+});
